86bec5d6d0618c516fe7b01084266bc90dab7cc6
Dave Marshall <mail@jorviksolutions.com>
2018-11-15 14:46:16 +0000

    initial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Dave Marshall on 15/11/2018.
+//  Copyright © 2018 Jorvik Solutions Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
